ef306f9298ce97dfdd3cfb5ae4ae88064b21f2a2
Oliver Lorenz <mail@oliverlorenz.com>
2020-09-15 22:24:10 +0200

    BREAKING CHANGE: adds new threshold functionality, adds test

---
+++ src/index.spec.ts
@@ -0,0 +1,217 @@
+import { VoiceChannelObserver, Counts } from ".";
+import { EventEmitter } from "events";
+import { Client, VoiceState } from "discord.js";
+
+describe("VoiceChannelObserver", () => {
+  const fakeChannelId = "myChannelId";
+  function buildObserver(existingUsers?: string[]) {
+    const emitter = new EventEmitter();
+    const observer = new VoiceChannelObserver(
+      emitter as Client,
+      fakeChannelId,
+      existingUsers
+    );
+    return {
+      observer,
+      emitter,
+    };
+  }
+
+  beforeEach(() => {});
+
+  describe("to be defined", () => {
+    it("with no users", () => {
+      const { observer } = buildObserver();
+      expect(observer).toBeDefined();
+    });
+
+    it("with predefined users", () => {
+      const { observer } = buildObserver(["fakeUser"]);
+      expect(observer).toBeDefined();
+    });
+  });
+
+  describe("onIncreased", () => {
+    it("changes parameters after join to channel", (done) => {
+      const { observer, emitter } = buildObserver();
+      observer.onIncreased((counts: Counts) => {
+        expect(counts).toEqual({ now: 1, before: 0 });
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        } as VoiceState,
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        } as VoiceState
+      );
+    });
+  });
+
+  describe("onDecreased", () => {
+    it("changes parameters after leave to channel", (done) => {
+      const { observer, emitter } = buildObserver(["fakeUser"]);
+
+      observer.onDecreased((changeObject) => {
+        expect(changeObject).toEqual({ now: 0, before: 1 });
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          channelID: fakeChannelId,
+        },
+        {
+          id: "fakeUser",
+          channelID: "TO_CHANNEL_ID",
+        }
+      );
+    });
+  });
+
+  describe("onEmpty", () => {
+    it("called if count drops to empty", (done) => {
+      const { observer, emitter } = buildObserver(["fakeUser"]);
+
+      observer.onEmpty((changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        },
+        {
+          id: "fakeUser",
+          channelID: "TO_CHANNEL_ID",
+        }
+      );
+    });
+  });
+
+  describe("onNotEmpty", () => {
+    it("called if count drops to empty", (done) => {
+      const { observer, emitter } = buildObserver();
+
+      observer.onNotEmpty((changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        },
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        }
+      );
+    });
+  });
+
+  describe("onChange", () => {
+    it("called if count increase", (done) => {
+      const { observer, emitter } = buildObserver();
+
+      observer.onChange((changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        },
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        }
+      );
+    });
+    it("called if count decrease", (done) => {
+      const { observer, emitter } = buildObserver(["fakeUser"]);
+
+      observer.onChange((changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        },
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        }
+      );
+    });
+  });
+  describe("onThresholdReached", () => {
+    it("callback if threshold is reached", (done) => {
+      const { observer, emitter } = buildObserver(["anotherFakeUser"]);
+      observer.onThresholdReached(2, (changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        },
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        }
+      );
+    });
+  });
+  describe("onThresholdLeft", () => {
+    it("callback if threshold is left", (done) => {
+      const { observer, emitter } = buildObserver([
+        "fakeUser",
+        "anotherFakeUser",
+      ]);
+      observer.onThresholdLeft(2, (changeObject) => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        },
+        {
+          id: "fakeUser",
+          channelID: "FROM_CHANNEL_ID",
+        }
+      );
+    });
+  });
+
+  describe("onNothingChanged", () => {
+    it("callback if nothing changed", (done) => {
+      const { observer, emitter } = buildObserver(["fakeUser"]);
+      observer.onNothingChanged(() => {
+        done();
+      });
+      emitter.emit(
+        "voiceStateUpdate",
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        },
+        {
+          id: "fakeUser",
+          channelID: fakeChannelId,
+        }
+      );
+    });
+  });
+});
